bed1f99c43ede09449990d653af916aba1c23470
eraldo <eraldo.borel@infoglobo.com.br>
2018-01-16 01:49:56 -0200

    Aula 47

---
+++ backend/config/database.js
@@ -0,0 +1,7 @@
+/**
+ * Created by eraldo on 16/01/2018.
+ */
+
+const mongoose = require('mongoose')
+
+module.exports = mongoose.connect('mongodb://localhost/db_finance')
--- backend/loader.js
+++ backend/loader.js
@@ -1,30 +1,5 @@
-/**
- * Created by eraldo on 14/01/2018.
- */
+require('./config/server')
 
-const port = 3003
-
-const bodyParser = require('body-parser')
-const express = require('express')
-const server = express()
-
-server.use(bodyParser.urlencoded({extended: true}))
-server.use(bodyParser.json())
-
-server.listen(port, function () {
-    console.log(`BACKEND is running on port ${port}.`)
-})
-
-
-server.use(function (req, res, next) {
-    console.log("meu middleware 1")
-    next()
-})
-
-server.use(function (req, res, next) {
-    console.log("meu middleware 2")
-    res.send("Funcionou novamente!!")
-})
-
+require('./config/database')
 
 
