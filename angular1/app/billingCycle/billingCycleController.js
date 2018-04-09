39c1f3a454b91e2ae222072df8fd9583b2d0a036
eraldo <eraldo.borel@infoglobo.com.br>
2018-04-08 23:57:55 -0300

    Inicio de listagem de ciclos

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -7,12 +7,22 @@
 
     function BillingCycleController($http, msgs) {
         const vm = this
+        const url = 'http://localhost:3003/api/billingCycles'
+
+        vm.refresh = function () {
+            $http.get(url)
+                .then(function (response) {
+                    const {data} = response
+                    vm.billingCycle = {}
+                    vm.billingCycles = data
+
+                })
+        }
 
         vm.create = function () {
-            const url = 'http://localhost:3003/api/billingCycles'
             $http.post(url, vm.billingCycle)
                 .then(function (response) {
-                    vm.billingCycle = {}
+                    vm.refresh()
                     msgs.addSuccess('Operação realizada com sucesso!')
                 })
                 .catch(function (response) {
@@ -20,5 +30,7 @@
                     msgs.addError(data.errors)
                 })
         }
+
+        vm.refresh()
     }
 })()
