e3bdb1ddcfdf246b18198387bc5767f66a862803
eraldo <eraldo.borel@infoglobo.com.br>
2018-04-16 19:52:02 -0300

    ativando funcionamento de abas

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -2,10 +2,11 @@
     angular.module('primeiraApp').controller('BillingCycleCtrl', [
         '$http',
         'msgs',
+        'tabs',
         BillingCycleController
     ])
 
-    function BillingCycleController($http, msgs) {
+    function BillingCycleController($http, msgs, tabs) {
         const vm = this
         const url = 'http://localhost:3003/api/billingCycles'
 
@@ -15,7 +16,7 @@
                     const {data} = response
                     vm.billingCycle = {}
                     vm.billingCycles = data
-
+                    tabs.show(vm, {tabList: true, tabCreate: true})
                 })
         }
 
@@ -29,6 +30,16 @@
                     const {data} = response
                     msgs.addError(data.errors)
                 })
+        }
+
+        vm.showTabUpdate = function (billingCycle) {
+            vm.billingCycle = billingCycle
+            tabs.show(vm, {tabUpdate: true})
+        }
+
+        vm.showTabDelete = function (billingCycle) {
+            vm.billingCycle = billingCycle
+            tabs.show(vm, {tabDelete: true})
         }
 
         vm.refresh()
