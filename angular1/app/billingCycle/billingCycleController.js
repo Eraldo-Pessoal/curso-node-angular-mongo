5821ac9db2e6ea6d81cad561c32eca17b9d2c685
eraldo <eraldo.borel@infoglobo.com.br>
2018-04-21 01:14:58 -0300

    Exibir resumo dentro das telas de ciclo de pagamento

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -19,6 +19,7 @@
                         debts: [{}],
                     }
                     vm.billingCycles = data
+                    vm.calculateValues()
                     tabs.show(vm, {tabList: true, tabCreate: true})
                 })
         }
@@ -37,11 +38,13 @@
 
         vm.showTabUpdate = function (billingCycle) {
             vm.billingCycle = billingCycle
+            vm.calculateValues()
             tabs.show(vm, {tabUpdate: true})
         }
 
         vm.showTabDelete = function (billingCycle) {
             vm.billingCycle = billingCycle
+            vm.calculateValues()
             tabs.show(vm, {tabDelete: true})
         }
 
@@ -77,11 +80,13 @@
 
         vm.cloneCredit = (index, {name, value}) => {
             vm.billingCycle.credits.splice(index + 1, 0, {name, value})
+            vm.calculateValues()
         }
 
         vm.deleteCredit = (index) => {
             if (vm.billingCycle.credits.length > 1) {
                 vm.billingCycle.credits.splice(index, 1)
+                vm.calculateValues()
             }
         }
 
@@ -92,12 +97,31 @@
 
         vm.cloneDebt = (index, {name, value, status}) => {
             vm.billingCycle.debts.splice(index + 1, 0, {name, value, status})
+            vm.calculateValues()
         }
 
         vm.deleteDebt = (index) => {
             if (vm.billingCycle.debts.length > 1) {
                 vm.billingCycle.debts.splice(index, 1)
+                vm.calculateValues()
             }
+        }
+
+        vm.calculateValues = () => {
+            vm.credit = 0
+            vm.debt = 0
+
+            if (vm.billingCycle) {
+                vm.billingCycle.credits.forEach(({value}) => {
+                    vm.credit += !value || isNaN(value) ? 0 : parseFloat(value)
+                })
+
+                vm.billingCycle.debts.forEach(({value}) => {
+                    vm.debt += !value || isNaN(value) ? 0 : parseFloat(value)
+                })
+            }
+
+            vm.total = vm.credit - vm.debt
         }
 
         vm.refresh()
